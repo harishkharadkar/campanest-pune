03c0941eaecaeb58c34efc31bb0290e6adebf911
campanest7-ops <campanest7@gmail.com>
2026-04-17 11:59:01 +0530

    fix service worker cache issue

--- public/sw.js
+++ public/sw.js
@@ -1,38 +1,13 @@
-const CACHE_NAME = 'campanest-v1';
-const ASSETS = [
-  '/',
-  '/index.html',
-  '/manifest.json'
-];
-
 self.addEventListener('install', (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(ASSETS);
-    })
-  );
+  self.skipWaiting();
 });
 
-self.addEventListener('fetch', (event) => {
-  if (event.request.method !== 'GET') {
-    return;
-  }
-
-  const requestUrl = new URL(event.request.url);
-  if (!['http:', 'https:'].includes(requestUrl.protocol)) {
-    return;
-  }
-
-  event.respondWith(
-    caches.match(event.request).then((response) => {
-      if (response) {
-        return response;
-      }
-
-      return fetch(event.request);
-    }).catch(() => {
-      // Avoid crashing fetch events for requests that are blocked/offline.
-      return new Response('Offline', { status: 503, statusText: 'Offline' });
-    })
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cache) => caches.delete(cache))
+      );
+    }).then(() => self.clients.claim())
   );
 });
